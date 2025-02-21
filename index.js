d3f9c19112acf496305b403a96ff412f4dae258d
Thevidu Sathsara Gamage <thevidu2007627@gmail.com>
2025-02-21 23:31:46 +0530

    Complete project

--- index.js
+++ index.js
@@ -21,23 +21,69 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-async function  getVisitedCountries (){
-  let countryArray = new Array()
-  const visitedCountriesResult = await db.query("SELECT country_code FROM visited_countries")
-  visitedCountriesResult.rows.forEach(row => {
-    countryArray.push(row["country_code"])
+async function getVisitedCountries() {
+  let countryArray = new Array();
+  const visitedCountriesResult = await db.query(
+    "SELECT country_code FROM visited_countries;"
+  );
+  visitedCountriesResult.rows.forEach((row) => {
+    countryArray.push(row["country_code"]);
   });
-  return countryArray
+  return countryArray;
 }
 
 app.get("/", async (req, res) => {
-
-  console.log()
   let visitedCountries = await getVisitedCountries();
   res.render("index.ejs", {
     countries: visitedCountries,
     total: visitedCountries.length,
   });
+});
+
+app.post("/add", async (req, res) => {
+  let userInput = req.body["country"].toLowerCase();
+
+  // Checks if user entered a value to input field or not
+  if (userInput.length > 1) {
+    let newCountryCode = "LK";
+    try {
+      // Try to run SQL code for catch any errors
+      const getCountryCode = await db.query(
+        "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+        [userInput]
+      );
+      // if SQL code runs properly assigning result value to variable
+      newCountryCode = getCountryCode.rows[0].country_code;
+    } catch (error) {
+      // if SQL query gives an error print that error message and handle it
+      console.log(error);
+      let visitedCountries = await getVisitedCountries();
+      res.render("index.ejs", {
+        countries: visitedCountries,
+        total: visitedCountries.length,
+        error: "Please Check Your Spellings and try again",
+      });
+    }
+
+    try {
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1);", [
+        newCountryCode,
+      ]);
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+      let visitedCountries = await getVisitedCountries();
+      res.render("index.ejs", {
+        countries: visitedCountries,
+        total: visitedCountries.length,
+        error: "Country already Added",
+      });
+    }
+
+    // res.send("Hello Princess");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
