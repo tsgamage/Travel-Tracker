17f968170b24995349cc168cc26886f0ccca6608
Thevidu Sathsara Gamage <thevidu2007627@gmail.com>
2025-02-21 22:16:17 +0530

    Add a get method for load homepage

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import express from "express";
+import express, { query } from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = new pg.Client({
-  host : localhost
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -12,8 +21,23 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function  getVisitedCountries (){
+  let countryArray = new Array()
+  const visitedCountriesResult = await db.query("SELECT country_code FROM visited_countries")
+  visitedCountriesResult.rows.forEach(row => {
+    countryArray.push(row["country_code"])
+  });
+  return countryArray
+}
+
 app.get("/", async (req, res) => {
-  //Write your code here.
+
+  console.log()
+  let visitedCountries = await getVisitedCountries();
+  res.render("index.ejs", {
+    countries: visitedCountries,
+    total: visitedCountries.length,
+  });
 });
 
 app.listen(port, () => {
